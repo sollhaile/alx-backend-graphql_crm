7ecd60f623e0ec840bbd9bf139ede704b2b1dc19
sollhaile <sollhaile192@gmail.com>
2025-07-08 22:24:44 +1200

    Intial Commit

---
+++ alx_backend_graphql_crm/schema.py
@@ -0,0 +1,7 @@
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, ALX!")
+
+# 👇 This must exist — this is what Graphene is looking for!
+schema = graphene.Schema(query=Query)
